56312fbf0c05a297ad53023314f135adf11ea7ab
ADARSH9358 <122091788+ADARSH9358@users.noreply.github.com>
2023-02-10 00:54:24 +0530

    Add files via upload

---
+++ two_sum_map.cpp
@@ -0,0 +1,14 @@
+ vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int>v;
+        unordered_map<int,int>mp;
+        for(int i=0;i<nums.size();i++){
+           if(mp.find(target-nums[i])!=mp.end()){
+               v.push_back(i);
+               v.push_back(mp[target-nums[i]]);
+           }
+           else
+           mp[nums[i]]=i;
+        }
+        return v;
+    }
+    //link is-->https://leetcode.com/problems/two-sum/
